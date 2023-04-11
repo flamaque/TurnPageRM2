06d3545733ebd92b4a0b1d2f6fd90b7d144a9a4a
flamaque <98748573+flamaque@users.noreply.github.com>
2023-04-11 16:52:49 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#to be run on the device
+wget https://github.com/flamaque/TurnPageRM2/archive/refs/tags/teste.tar.gz -O - | tar xvf -
+cp /home/root/touch.service /etc/systemd/system
+systemctl enable touch
+systemctl start touch
